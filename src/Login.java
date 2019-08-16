09b8156072259be05f0d5fabd1c0326437aaa8b4
yihong0906 <tehyh-wm18@student.tarc.edu.my>
2019-08-16 09:26:27 +0800

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -2,48 +2,61 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Login {
 
-	public void login()
+	public static String login()
 	{   
 		ArrayList<StaffDetails>staff=Staff.staffs;
 		Scanner input=new Scanner(System.in);
 	    String[] password= {"hxkiller03","hxkiller04","hxkiller05","hxkiller06","hxkiller07","hxkiller08"};
-	    int check=8,valid=3,checking,variable1=1,variable2=0;
-	    String position;
+	    int check=8,valid=3,checking,usernameIndex=1,passwordIndex=0;
+	    String position="0";
+	    String fakePosition="0";
 	    
 		System.out.println("\r\n" + 
-				"  ______      _____ _______     __  __          _____ _______ \r\n" + 
-				" |  ____/\\   |  __ \\__   __|   |  \\/  |   /\\   |  __ \\__   __|\r\n" + 
-				" | |__ /  \\  | |__) | | |      | \\  / |  /  \\  | |__) | | |   \r\n" + 
-				" |  __/ /\\ \\ |  _  /  | |      | |\\/| | / /\\ \\ |  _  /  | |   \r\n" + 
-				" | | / ____ \\| | \\ \\  | |      | |  | |/ ____ \\| | \\ \\  | |   \r\n" + 
-				" |_|/_/    \\_\\_|  \\_\\ |_|      |_|  |_/_/    \\_\\_|  \\_\\ |_|   \r\n" + 
+				"\t\t\t\t\t\t\t\t\t  ______      _____ _______     __  __          _____ _______ \r\n" + 
+				"\t\t\t\t\t\t\t\t\t |  ____/\\   |  __ \\__   __|   |  \\/  |   /\\   |  __ \\__   __|\r\n" + 
+				"\t\t\t\t\t\t\t\t\t | |__ /  \\  | |__) | | |      | \\  / |  /  \\  | |__) | | |   \r\n" + 
+				"\t\t\t\t\t\t\t\t\t |  __/ /\\ \\ |  _  /  | |      | |\\/| | / /\\ \\ |  _  /  | |   \r\n" + 
+				"\t\t\t\t\t\t\t\t\t | | / ____ \\| | \\ \\  | |      | |  | |/ ____ \\| | \\ \\  | |   \r\n" + 
+				"\t\t\t\t\t\t\t\t\t |_|/_/    \\_\\_|  \\_\\ |_|      |_|  |_/_/    \\_\\_|  \\_\\ |_|   \r\n" + 
 				"                                                              \r\n" + 
 				"                                                              \r\n" + 
 				"");
 		
-		System.out.println("Welcome to the Fart Mart Point of sale system,now please enter your username and password.");
+		
+			
+		    System.out.println("\n                                        +===============================================+");
+			System.out.println("                                        | Welcome to the Fart Mart Point of sale system   |\n" +
+							   "                                        |   Now please enter your username and password,  |\n"+
+					 		   "                                        |					(0 to exit)	     			  |");
+			System.out.println("                                        +===============================================+");
+			
 		do {
-		System.out.print("Username :");
+		System.out.print("\t\t\t\t\t\t\t\t\t\t\t\tUsername : ");
 		String username=input.nextLine();
-		System.out.print("Password :");
+		if (username.equals("0"))
+			return "0";
+		System.out.print("\n\t\t\t\t\t\t\t\t\t\t\t\tPassword : ");
 		String pass=input.nextLine();
 		
 		for(checking=0;checking<staff.size();checking++)
 		{
 			if(username.equals(staff.get(checking).getUserName()))
 				{
-				variable1=checking;
+				usernameIndex=checking;
 				}
 		}
 		for(checking=0;checking<password.length;checking++)
 		{
 			if(pass.equals(password[checking]))
-				{variable2=checking;
+				{
+					passwordIndex=checking;
 				}
 		}
-		if(variable1==variable2)
+		if(usernameIndex==passwordIndex)
 		{
 			check=0;
+			
+		
 		}	
 		if(check!=0)
 		{
@@ -56,12 +69,15 @@ public class Login {
 		}while(check!=0);
 		
 		
-		System.out.println("1.Staff Details");
-		System.out.println("2.Product Details");
-		System.out.println("3.Accept Order");
-		System.out.println("4.Memberships");
-		System.out.println("5.Reports");
 		
+
+		
+		if(staff.get(usernameIndex).getPosition().equals("Staff")){
+			position="Staff";	
+			}else
+			position="Manager";	
+		
+		return position;
 	}
 	
 }
