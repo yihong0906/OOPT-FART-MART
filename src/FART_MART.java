d1b18b39390fba91295f078367d975168244869f
benjamincxw <54138801+benjamincxw@users.noreply.github.com>
2019-08-17 03:13:47 +0800

    Update FART_MART.java

--- src/FART_MART.java
+++ src/FART_MART.java
@@ -36,7 +36,7 @@ public class FART_MART {
 		    System.out.println("\n                                        +===============================================+");
 			System.out.println("                                        |           	WELCOME TO FART MART!           |");
 			System.out.println("                                        +===============================================+");
-			System.out.println("                                        |1. Make Transaction                            |");
+			System.out.println("                                        |1. Accept Sales                                |");
 			System.out.println("                                        |2. Products Module                             |");
 			System.out.println("                                        |3. Staff Details                               |");
 			System.out.println("                                        |4. Membership Module                           |");
@@ -52,8 +52,6 @@ public class FART_MART {
 	    		
 	    	  	if(choice == 1){
 	    			accept_sales.ACCEPT_SALES();
-	    			// make transaction
-	
 	    		} else if (choice ==2){
 	    			
 	    			prod_details.PRODUCT_DETAILS_MODULE(position); 
